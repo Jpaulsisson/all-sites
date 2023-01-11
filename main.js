4bc4a82d8a6ea0cae535ae4e2add37188db97695
Paul Sisson <paulsissonsemail@gmail.com>
2023-01-11 13:34:49 -0600

    added to all

---
+++ main.js
@@ -0,0 +1,30 @@
+/* Main javascript file */
+
+function generateRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 5);
+  return randomNumber;
+};
+console.log(generateRandomNumber());
+
+const randomSiteGame = () => {
+  generateRandomNumber();
+  switch(randomNumber) {
+    case 0:
+      //link to site;
+      break;
+    case 1:
+      //link to site;
+      break;
+    case 2:
+      //link to site;
+      break;
+    case 3:
+      //link to site;
+      break;
+    case 4:
+      //link to site;
+      break;
+    default:
+      console.log(`something went wrong`);
+  ;}
+};
