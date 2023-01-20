5d6369b9b601f021c0b3a209856a869dec5d8b6f
Paul Sisson <paulsissonsemail@gmail.com>
2023-01-20 14:23:20 -0600

    big JS improvements

--- main.js
+++ main.js
@@ -1,1 +1,92 @@
-141, 209, 193
+/* ==================== MAIN JAVASCRIPT FILE ===================== */
+
+/* ==================== VARIABLE BANK ============================ */
+
+const hello = document.getElementById('hello-sign');
+const logo = document.getElementById('logo');
+const welcomeP = document.getElementById('welcome-p');
+const aboutMeP = document.querySelector('.about-me-p');
+const aboutSiteP = document.querySelector('.about-site-p');
+const otherProjectsP = document.querySelector('.other-projects-p');
+
+
+/*============== PARAGRAPH INTERACTIVITY FUNCTIONS ================ */
+
+
+function changeWelcomePColor() {
+  if (welcomeP.style.color !== 'orange') {
+    welcomeP.style.color = 'orange';
+  } else {
+    welcomeP.style.color = 'rgb(32, 29, 29)';
+  };
+};
+
+welcomeP.onclick = changeWelcomePColor;
+
+function changeAboutMePColor() {
+  if (aboutMeP.style.color !== 'orange') {
+    aboutMeP.style.color = 'orange';
+  } else {
+    aboutMeP.style.color = 'rgb(32, 29, 29)';
+  };
+};
+
+aboutMeP.onclick = changeAboutMePColor;
+
+function changeAboutSitePColor() {
+  if (aboutSiteP.style.color !== 'orange') {
+    aboutSiteP.style.color = 'orange';
+  } else {
+    aboutSiteP.style.color = 'rgb(32, 29, 29)';
+  };
+};
+
+aboutSiteP.onclick = changeAboutSitePColor;
+
+function changeOtherProjectsPColor() {
+  if (otherProjectsP.style.color !== 'orange') {
+    otherProjectsP.style.color = 'orange';
+  } else {
+    otherProjectsP.style.color = 'rgb(32, 29, 29)';
+  };
+};
+
+otherProjectsP.onclick = changeOtherProjectsPColor;
+
+
+/*===============================END OF PARAGRAPH INTERACTIVITY=======================================*/ 
+/*===============================Start of welcome sign functions======================================*/
+
+
+function makeBright() {
+  if (hello.style.filter === 'none'){
+    hello.style.filter = 'brightness(150%)';
+    hello.style.background = 'radial-gradient(closest-side, rgb(0, 0, 0, 0.5), rgb(0, 0, 0, 0.5), rgb(0, 0, 0, 0.5), rgba(255, 105, 180, 0.7), rgba(255, 105, 180, 0.25), rgba(255, 105, 180, 0.05), rgb(0, 0, 0, 0.5), rgb(0, 0, 0, 0.5))';
+  } else {
+    hello.style.filter = 'none';
+    hello.style.background = 'none';
+  }
+};
+
+hello.onclick = makeBright;
+
+function makeShrink() {
+  hello.style.width = '99%';
+};
+
+function returnToNormalSize() {
+  hello.style.width = '100%';
+};
+
+hello.onmousedown = makeShrink;
+hello.onmouseup = returnToNormalSize;
+
+function changeSize() {
+  if (element.style.fontSize === '72px') {
+    element.style.fontSize = '120px';
+  } else {
+    element.style.fontSize = '72px';
+  }
+};
+
+logo.onclick = changeSize();
